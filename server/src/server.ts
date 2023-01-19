fe26d5267afc40c90696d08d20aeb569edc964aa
William Silva <william.faria@rarolabs.com.br>
2023-01-19 15:53:44 -0300

    Aula 02: back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,12 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
+import { appRoutes } from './routes';
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
+app.register(appRoutes);
 
-app.get('/', async () => {
-    const habits = await prisma.habit.findMany({
-        where: {
-            title: {
-                startsWith: 'Teste'
-            }
-        }
-    });
-    return habits
-});
 
 app.listen({
     port: 3333

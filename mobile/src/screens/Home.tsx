264894998b22ace7cd67596ea37a742067bcab7c
William Silva <william.faria@rarolabs.com.br>
2023-01-19 18:28:24 -0300

    Aula 02: mobile

---
+++ mobile/src/screens/Home.tsx
@@ -0,0 +1,66 @@
+import { ScrollView, Text, View } from "react-native";
+import { DAY_SIZE, HabitDay } from "../components/HabitDay";
+import { Header } from "../components/Header";
+import { generateRangeDatesFromYearStart } from "../utils/generateRangeDatesFromYearStart";
+
+const weekDays = [
+    'D',
+    'S',
+    'T',
+    'Q',
+    'Q',
+    'S',
+    'S'
+];
+
+const summaryDates = generateRangeDatesFromYearStart();
+const minimumSummaryDatesSize = 18 * 5;
+const amoutOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
+
+export function Home() {
+    return (
+        <View className="flex-1 bg-background px-8 pt-16">
+            <Header/>
+
+            <View className="flex-row mt-6 mb-2">
+                {
+                    weekDays.map((weekDay, index) => (
+                        <Text
+                            className="text-zinc-400 text-xl font-bold text-center mx-1"
+                            style={{width: DAY_SIZE}}
+                            key={`${weekDay}-${index}`}>
+                            {weekDay}
+                        </Text>
+                    ))
+                }
+            </View>
+
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{paddingBottom: 100}}>
+                <View className="flex-row flex-wrap">
+                    {
+                        summaryDates.map(date => (
+                            <HabitDay
+                                key={date.toISOString()}
+                            />
+                        ))
+                    }
+
+                    {
+                        amoutOfDaysToFill > 0 && Array.from({length: amoutOfDaysToFill}).map((_, index) => (
+                            <View
+                                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                                style={{width: DAY_SIZE, height: DAY_SIZE}}
+                                key={index}
+                            />
+                        ))
+                    }
+                </View>
+            </ScrollView>
+
+            
+            <HabitDay/>
+        </View>
+    )
+}

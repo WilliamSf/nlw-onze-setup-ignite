d8d9084cf2002ac2924ae51a9a793c2355435900
William Silva <william.faria@rarolabs.com.br>
2023-01-21 22:05:52 -0300

    Aula 03: mobile

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -3,15 +3,20 @@ import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
 
 import Logo from "../assets/logo.svg";
+import { useNavigation } from "@react-navigation/native";
 
 export function Header() {
+    const { navigate } = useNavigation(); 
+
     return (
         <View className="w-full flex-row items-center justify-between">
             <Logo/>
 
             <TouchableOpacity
                 className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
-                activeOpacity={0.7}>
+                activeOpacity={0.7}
+                onPress={() => navigate('new')}
+            >
                 <Feather
                     name="plus"
                     color={colors.violet[500]}
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,66 +1,64 @@
-import { ScrollView, Text, View } from "react-native";
-import { DAY_SIZE, HabitDay } from "../components/HabitDay";
-import { Header } from "../components/Header";
-import { generateRangeDatesFromYearStart } from "../utils/generateRangeDatesFromYearStart";
+import { Text, View, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-const weekDays = [
-    'D',
-    'S',
-    'T',
-    'Q',
-    'Q',
-    'S',
-    'S'
-];
 
-const summaryDates = generateRangeDatesFromYearStart();
-const minimumSummaryDatesSize = 18 * 5;
-const amoutOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
+import { Header } from '../components/Header';
+import { HabitDay, DAY_SIZE } from '../components/HabitDay';
+import { generateRangeDatesFromYearStart } from '../utils/generateRangeDatesFromYearStart';
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+const datesFromYearStart = generateRangeDatesFromYearStart();
+const minimunSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length
 
 export function Home() {
-    return (
-        <View className="flex-1 bg-background px-8 pt-16">
-            <Header/>
+  const { navigate } = useNavigation();
+  return (
+    <View className='flex-1 bg-background px-8 pt-16'>
+      <Header />
 
-            <View className="flex-row mt-6 mb-2">
-                {
-                    weekDays.map((weekDay, index) => (
-                        <Text
-                            className="text-zinc-400 text-xl font-bold text-center mx-1"
-                            style={{width: DAY_SIZE}}
-                            key={`${weekDay}-${index}`}>
-                            {weekDay}
-                        </Text>
-                    ))
-                }
-            </View>
+      <View className="flex-row mt-6 mb-2">
+        {
+          weekDays.map((weekDay, i) => (
+            <Text 
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl font-bold text-center mx-1"
+              style={{ width: DAY_SIZE }}
+            >
+              {weekDay}
+            </Text>
+          ))
+        }
+      </View>
 
-            <ScrollView
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={{paddingBottom: 100}}>
-                <View className="flex-row flex-wrap">
-                    {
-                        summaryDates.map(date => (
-                            <HabitDay
-                                key={date.toISOString()}
-                            />
-                        ))
-                    }
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className='flex-row flex-wrap'>
+          {
+            datesFromYearStart.map(date => (
+              <HabitDay 
+                key={date.toISOString()}
+                onPress={() => navigate('habit', { date: date.toISOString() })}
+              />
+            ))
+          }
 
-                    {
-                        amoutOfDaysToFill > 0 && Array.from({length: amoutOfDaysToFill}).map((_, index) => (
-                            <View
-                                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
-                                style={{width: DAY_SIZE, height: DAY_SIZE}}
-                                key={index}
-                            />
-                        ))
-                    }
-                </View>
-            </ScrollView>
-
-            
-            <HabitDay/>
+          {
+            amountOfDaysToFill > 0 && Array
+            .from({ length: amountOfDaysToFill })
+            .map((_, index) => (
+              <View 
+                key={index}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))
+          }
         </View>
-    )
-}
+      </ScrollView>
+
+    </View>
+  )
+} 
